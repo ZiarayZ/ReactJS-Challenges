c0270f2d2113a910fd9644e8090e4931f092a74f
ZiarayZ <jakesmith@binaryrage.com>
2022-09-29 11:53:52 +0100

    first attempt

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
 
 function App() {
+  const [theme, setTheme] = useState('light-mode');
+  const [icon, setIcon] = useState(faMoon);
+  const themeToggler = () => {
+    theme === 'light-mode' ? setTheme('dark-mode') : setTheme('light-mode');
+    icon === faMoon ? setIcon(faSun) : setIcon(faMoon);
+    //could probably combine these in some way
+  }
   return (
     <div className="app">
       <div className="level">
@@ -12,8 +19,8 @@ function App() {
         </div>
 
         {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+        <button className="app__dark-mode-btn icon level-right" onClick={themeToggler}>
+          <FontAwesomeIcon icon={icon} color="#FFA500" />
         </button>
 
       </div>
